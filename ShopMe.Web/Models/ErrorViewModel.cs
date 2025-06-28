708a8e8af0e1784cc36ece7823ca99a44a2e15cf
Hossam Hosny <HossamHosny415@gmail.com>
2025-06-28 13:18:44 +0300

    Initializing App Infrastructure

---
+++ ShopMe.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ShopMe.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
