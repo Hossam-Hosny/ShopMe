0099ebc8d17a8e0d740eb45985dd416fd445a18b
Hossam Hosny <HossamHosny415@gmail.com>
2025-07-11 15:37:58 +0300

    Implementing Customer Table Page Showing to Admin role

---
+++ ShopMe.Web/Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopMe.DataAccess.Context;
+using System.Security.Claims;
+
+namespace ShopMe.Web.Areas.Admin.Controllers;
+[Area("Admin")]
+public class UsersController(AppDbContext _db) : Controller
+{
+    public IActionResult Index()
+    {
+        var claimsIdentity =(ClaimsIdentity) User.Identity;
+        var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+        string userId = claim.Value;
+
+
+
+        return View(_db.appUsers.Where(x=>x.Id != userId).ToList());
+    }
+}
